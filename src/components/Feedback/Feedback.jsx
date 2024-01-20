d6ec8bbaf4c5db409c3b19c17229e9474f2f54f8
inhasid <sidorchuk.inga@gmail.com>
2024-01-20 19:33:15 +0100

    Hooks

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, Component } from "react";
 
 import SectionTitle from "./SectionTitle/SectionTitle";
 import NotificationMessage from "./NotificationMessage/NotificationMessage";
@@ -7,7 +7,61 @@ import Statistics from "./Statistics/Statistics";
 
 import styles from "./feedback.module.css";
 
-class Feedback extends Component {
+const feedbackOptions = ["Good", "Neutral", "Bad"];
+
+const Feedback = () => {
+    const [feedback, setFeedback] = useState({
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    });
+
+    const countTotalFeedback = () =>
+    {
+        const { good, neutral, bad } = feedback;
+        const total = good + neutral + bad;
+        return total;
+    }
+
+    const countPositiveFeedbackPercentage = (keyName) => {
+        const total = countTotalFeedback();
+        if (!total) {
+            return 0;
+        }
+        const value = feedback[keyName];
+        return Number(((value / total) * 100).toFixed(2));
+    }
+
+    const leaveFeedback = (keyName) => {
+        const lowerCaseKey = keyName.toLowerCase();
+        setFeedback(prevFeedback => ({
+            ...prevFeedback,
+            [lowerCaseKey]: prevFeedback[lowerCaseKey] + 1 
+        }))
+    }
+
+    const { good, neutral, bad } = feedback;
+    const total = countTotalFeedback();
+    const positivePercentage = countPositiveFeedbackPercentage("good");
+
+    const isFeedbackGiven = total > 0;
+
+
+    return (
+            <div className={styles.wrapper}>
+                <SectionTitle title="Please leave feedback">
+                    <FeedbackOptions options={feedbackOptions} onLeaveFeedback={leaveFeedback} />
+                </SectionTitle>
+                <SectionTitle title="Statistics">
+                    {isFeedbackGiven ? (<Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage} />) :
+                        (<NotificationMessage message="There is no feedback" />)}
+                    
+                </SectionTitle>               
+            </div>
+            )
+}
+
+/*class Feedback extends Component {
     static feedbackOptions = ["Good", "Neutral", "Bad"]
 
     state = {
@@ -62,6 +116,6 @@ class Feedback extends Component {
             </div>
         )
      }
-}
+}*/
 
 export default Feedback;
--- src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
@@ -1,5 +1,7 @@
+import styles from "./feedback-options.module.css";
+
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-    const buttonElements = options.map(name => <button onClick={(() => onLeaveFeedback(name))} key={name}>{name}</button>);
+    const buttonElements = options.map(name => <button onClick={(() => onLeaveFeedback(name))} key={name} className={styles.btn}>{name}</button>);
 
     return buttonElements;
 }
