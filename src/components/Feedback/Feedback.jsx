8dd452ea68bac2b51994931946135730f073e745
inhasid <sidorchuk.inga@gmail.com>
2024-01-20 19:45:07 +0100

    hooks2

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,4 +1,4 @@
-import { useState, Component } from "react";
+import { useState } from "react";
 
 import SectionTitle from "./SectionTitle/SectionTitle";
 import NotificationMessage from "./NotificationMessage/NotificationMessage";
@@ -60,62 +60,5 @@ const Feedback = () => {
             </div>
             )
 }
-
-/*class Feedback extends Component {
-    static feedbackOptions = ["Good", "Neutral", "Bad"]
-
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-    }
-    
-    countTotalFeedback() {
-        const { good, neutral, bad } = this.state;
-        const total = good + neutral + bad;
-        return total;
-    }
-
-    countPositiveFeedbackPercentage(keyName) {
-        const total = this.countTotalFeedback();
-        if (!total) {
-            return 0;
-        }
-        const value = this.state[keyName];
-        return Number(((value / total) * 100).toFixed(2));
-    }
-
-    leaveFeedback = (keyName) => {
-        const lowerCaseKey = keyName.toLowerCase();
-        this.setState(prevState => {
-            return {
-                [lowerCaseKey]: prevState[lowerCaseKey] + 1
-            }
-        })
-    }
-
-    render() {
-        const { good, neutral, bad } = this.state;
-
-        const total = this.countTotalFeedback();
-        const positivePercentage = this.countPositiveFeedbackPercentage("good");
-
-        const isFeedbackGiven = total > 0;
-
-
-        return (
-            <div className={styles.wrapper}>
-                <SectionTitle title="Please leave feedback">
-                    <FeedbackOptions options={Feedback.feedbackOptions} onLeaveFeedback={this.leaveFeedback} />
-                </SectionTitle>
-                <SectionTitle title="Statistics">
-                    {isFeedbackGiven ? (<Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage} />) :
-                        (<NotificationMessage message="There is no feedback" />)}
-                    
-                </SectionTitle>               
-            </div>
-        )
-     }
-}*/
 
 export default Feedback;
